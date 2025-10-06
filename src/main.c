0bdcb89353037dfaf7a780314558a157b44d86e1
William East <weast@c1b2c2.42berlin.de>
2025-10-06 17:08:02 +0200

    added parser for the values at the beginning

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: weast <weast@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 12:55:59 by weast             #+#    #+#             */
-/*   Updated: 2025/10/06 14:45:17 by weast            ###   ########.fr       */
+/*   Updated: 2025/10/06 17:07:49 by weast            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,33 @@ int	main(int argc, char **argv)
 {
 	t_window	window;
 	t_map		map = {0};
+	t_config	config = {0};
+	int			offset;
 
 	if (argc != 2)
 	{
 		ft_putstr_fd("Usage: ./cub3d <map.cub>\n", 2);
 		return (1);
 	}
+	offset = parse_config_data(&config, argv[1]);
+	if (offset == -1)
+	{
+		ft_putstr_fd("Error: Missing or invalid config data\n", 2);
+		return (1);
+	}
+	map.y_offset = offset;
+	map.raw_map_string = config.raw_file_string + map.y_offset;
+	if (create_map(&map))
+		return (1);
 	if (init_window(&window) != 0)
 	{
 		ft_putstr_fd("Error: Failed to initialize game\n", 2);
 		return (1);
 	}
-	if (create_map(&map, argv[1])) /* TODO fix error handling and proper freeing. */
-		return (1);
-
 	mlx_key_hook(window.mlx, key_hook, &window);
 	mlx_loop(window.mlx);
 	cleanup_window(&window);
 	cleanup_map(&map);
+	cleanup_config(&config);
 	return (0);
 }
--- src/read_map.c
+++ src/read_map.c
@@ -6,7 +6,7 @@
 /*   By: weast <weast@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 16:30:28 by weast             #+#    #+#             */
-/*   Updated: 2025/10/06 15:08:57 by weast            ###   ########.fr       */
+/*   Updated: 2025/10/06 16:29:49 by weast            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,46 +17,48 @@
 #include <stdio.h>
 #include <unistd.h>
 
-static int	check_extension(char *filename)
-{
-	int	ext_len;
-	int	name_len;
+/* static int	check_extension(char *filename) */
+/* { */
+/* 	int	ext_len; */
+/* 	int	name_len; */
 
-	ext_len = ft_strlen(FILE_EXT);
-	name_len = ft_strlen(filename);
-	if (ext_len >= name_len)
-		return (0);
-	return (!ft_strncmp(FILE_EXT, &filename[name_len - ext_len], 5));
-}
+/* 	ext_len = ft_strlen(FILE_EXT); */
+/* 	name_len = ft_strlen(filename); */
+/* 	if (ext_len >= name_len) */
+/* 		return (0); */
+/* 	return (!ft_strncmp(FILE_EXT, &filename[name_len - ext_len], 5)); */
+/* } */
 
-static int	read_map_as_string(t_map *map, char *filename)
-{
-	int	fd;
-	char *line;
-	char *temp;
+/* static int	read_map_as_string(t_map *map, char *filename) */
+/* { */
+/* 	int	fd; */
+/* 	char *line; */
+/* 	char *temp; */
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0 || !check_extension(filename))
-		return (1);
-	map->raw_map_string = ft_strdup("");
-	if (!map->raw_map_string)
-		return (1);
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		temp = ft_strjoin(map->raw_map_string, line);
-		free(map->raw_map_string);
-		if (!temp)
-		{
-			free(line);
-			return (1);
-		}
-		map->raw_map_string = temp;
-		free(line);
-	}
-	close(fd);
-	return (0);
-}
+/* 	fd = open(filename, O_RDONLY); */
+/* 	if (fd < 0 || !check_extension(filename)) */
+/* 		return (1); */
+/* 	map->raw_map_string = ft_strdup(""); */
+/* 	if (!map->raw_map_string) */
+/* 		return (1); */
+/* 	while ((line = get_next_line(fd)) != NULL) */
+/* 	{ */
+/* 		temp = ft_strjoin(map->raw_map_string, line); */
+/* 		free(map->raw_map_string); */
+/* 		if (!temp) */
+/* 		{ */
+/* 			free(line); */
+/* 			return (1); */
+/* 		} */
+/* 		map->raw_map_string = temp; */
+/* 		free(line); */
+/* 	} */
+/* 	close(fd); */
+/* 	return (0); */
+/* } */
 
+// INFO: map object is bzeroed in main.
+// raw_map_string already points to start of map data
 static int	get_map_max_dimensions(t_map *map)
 {
 	int row_len;
@@ -64,6 +66,7 @@ static int	get_map_max_dimensions(t_map *map)
 
 	i = 0;
 	row_len = 0;
+	map->dimension.y = 0;
 	while (map->raw_map_string[i])
 	{
 		if (map->raw_map_string[i] == '\n')
@@ -83,6 +86,8 @@ static int	get_map_max_dimensions(t_map *map)
 	return (0);
 }
 
+
+// offset is used for validation start
 static int validate_map_chars(t_map *map)
 {
 	int i;
@@ -111,20 +116,13 @@ static int validate_map_chars(t_map *map)
 		return (0);
 }
 
-int	create_map(t_map *map, char *filename)
+int	create_map(t_map *map)
 {
-	ft_printf("Opening %s...\n", filename);
-	if (read_map_as_string(map, filename))
-		return(ft_putstr_fd("Invalid input file.\n", STDERR_FILENO));
 	if (validate_map_chars(map))
 		return(ft_putstr_fd("Invalid characters in map.", STDERR_FILENO));
 	get_map_max_dimensions(map);
-	ft_printf("Allocating %s...\n", filename);
 	if (allocate_flat_map(map))
 		return(ft_putstr_fd("Could not allocate map grid.\n", STDERR_FILENO));
-
-	ft_printf("%s loaded successfully.\n", filename);
 	debug_map(map);
 	return (0);
 }
-
