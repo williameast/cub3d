662930345464642a248c6aebf624c113185d2868
William East <weast@c1b2c2.42berlin.de>
2025-10-20 14:38:46 +0200

    added movement tracking and valiation

--- src/find_player.c
+++ src/find_player.c
@@ -6,7 +6,7 @@
 /*   By: dimachad <dimachad@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 19:26:42 by dimachad          #+#    #+#             */
-/*   Updated: 2025/10/14 18:50:57 by weast            ###   ########.fr       */
+/*   Updated: 2025/10/20 13:57:42 by weast            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,23 @@ void	find_player(char **map, double *player, int cols, int rows)
 		col = 0;
 		row++;
 	}
+}
+
+
+// right now I have made a bit more defensively coded
+// to check also bounds. but we can probably take that out.
+int	validate_player_move(t_map *map, double pos[2])
+{
+	int	grid_x;
+	int	grid_y;
+
+	grid_x = (int)pos[x];
+	grid_y = (int)pos[y];
+	if (grid_x < 0 || grid_x >= map->size[x])
+		return (INVALID);
+	if (grid_y < 0 || grid_y >= map->size[y])
+		return (INVALID);
+	if (map->grid[grid_y][grid_x] == '1')
+		return (INVALID);
+	return (OK);
 }
--- src/input_mapping.c
+++ src/input_mapping.c
@@ -6,16 +6,81 @@
 /*   By: weast <weast@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 14:25:40 by weast             #+#    #+#             */
-/*   Updated: 2025/10/07 14:31:20 by weast            ###   ########.fr       */
+/*   Updated: 2025/10/20 14:28:06 by weast            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MLX42/MLX42.h"
 #include "cub3d.h"
+
+enum	dir
+{
+	NORTH,
+	EAST,
+	SOUTH,
+	WEST
+};
+
+static void	calculate_new_position(int dir, double *new_x, double *new_y)
+{
+	if (dir == NORTH)
+	{
+		*new_x += MOVEMENT_VELOCITY;
+	}
+	else if (dir == SOUTH)
+	{
+		*new_x -= MOVEMENT_VELOCITY;
+	}
+	else if (dir == EAST)
+	{
+		*new_y += MOVEMENT_VELOCITY;
+	}
+	else if (dir == WEST)
+	{
+		*new_y -= MOVEMENT_VELOCITY;
+	}
+}
+
+void	handle_movement(int dir, t_game *game)
+{
+	double	new_x;
+	double	new_y;
+	double	temp[2];
+	static int first_call = 1;
+
+	if (first_call)
+	{
+		printf("FIRST CALL - Player pos before movement: (%.2f, %.2f)\n", game->player.pos[x], game->player.pos[y]);
+		first_call = 0;
+	}
+	new_x = game->player.pos[x];
+	new_y = game->player.pos[y];
+	calculate_new_position(dir, &new_x, &new_y);
+	if ((int)new_x != (int)game->player.pos[x]
+		|| (int)new_y != (int)game->player.pos[y])
+	{
+		temp[x] = new_x;
+		temp[y] = new_y;
+		if (validate_player_move(&game->map, temp) != OK)
+			return ;
+	}
+	game->player.pos[x] = new_x;
+	game->player.pos[y] = new_y;
+	printf("Player pos: (%.2f, %.2f)\n", new_x, new_y);
+}
 
 // this we use to handle key presses.
 void	key_hook(mlx_key_data_t key_press, void *param)
 {
-	t_window *window = (t_window *)param;
+	t_game *game = (t_game *)param;
 	if (key_press.key == MLX_KEY_ESCAPE && key_press.action == MLX_PRESS)
-		mlx_close_window(window->mlx);
+		mlx_close_window(game->win.mlx);
+	if (key_press.key == MLX_KEY_UP && key_press.action != MLX_RELEASE)
+		handle_movement(NORTH, game);
+	if (key_press.key == MLX_KEY_DOWN && key_press.action != MLX_RELEASE)
+		handle_movement(SOUTH, game);
+	if (key_press.key == MLX_KEY_LEFT && key_press.action != MLX_RELEASE)
+		handle_movement(EAST, game);
+	if (key_press.key == MLX_KEY_RIGHT && key_press.action != MLX_RELEASE)
+		handle_movement(WEST, game);
 }
